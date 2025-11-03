832420d01ab106f7a5a9d9a78ffb51b1dc0f5df6
David Gutierrez <dagd0918@gmail.com>
2025-11-03 07:01:10 -0300

    Adding the two cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+/* import { useState } from "react"; */
 
 import "./App.css";
 
@@ -6,7 +6,52 @@ function App() {
   return (
     <>
       <main>
-        <form action=""></form>
+        <div className="purpule-top-section">
+          <img
+            className="card-back"
+            src="src/images/bg-card-back.png"
+            alt="card back"
+          />
+          <div className="card-front-logo">
+            <img
+              className="card-front"
+              src="src/images/bg-card-front.png"
+              alt="card back"
+            />
+            <img
+              className="card-logo"
+              src="src/images/card-logo.svg"
+              alt="card logo"
+            />
+          </div>
+        </div>
+
+        <form action="">
+          {/* CARDHOLDER NAME */}
+          <label htmlFor="">
+            CARDHOLDER NAME
+            <input type="text" placeholder="e.g. Jane Applessed" />
+          </label>
+
+          {/* CARD NUMBER */}
+          <label htmlFor="">
+            CARD NUMBER
+            <input type="text" placeholder="e.g. 1234 5678 9123 0000" />
+          </label>
+
+          {/* Exp. date (mm/yy) */}
+          <label htmlFor="">
+            EXP. DATE (MM/YY)
+            <input type="text" placeholder="MM" />
+            <input type="text" placeholder="YY" />
+          </label>
+
+          {/* CVC */}
+          <label htmlFor="">
+            CVC
+            <input type="text" placeholder="e.g. 123" />
+          </label>
+        </form>
       </main>
     </>
   );
